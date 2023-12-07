604cecb7325709ed0cce290761187d559f2f581e
hexdhog <hexdhog@gmail.com>
2023-12-07 21:28:24 +0100

    2023 05 1 complete

--- 2023/05p01.py
+++ 2023/05p01.py
@@ -5,9 +5,27 @@ from __future__ import annotations
 import sys
 
 class Map:
-  def __init__(self, table: list[tuple[int, int, int]], child: Map | None = None): self.table, self.child = table, child
-  def resolve(self, value: int) -> int: return value
+  def __init__(self, table: list[tuple], child: Map | None = None): self.table, self.child = table, child
+  def resolve(self, value: int) -> int:
+    for dst, src, length in self.table:
+      if src <= value <= src + length:
+        value = value + dst - src
+        break
+    return value if self.child is None else self.child.resolve(value)
+
+def split(l: list, check: object) -> list:
+  ret, tmp = [], [] # type: ignore[var-annotated]
+  for e in l:
+    if e == check:
+      if len(tmp) > 0: ret.append(tmp)
+      tmp = []
+    else:
+      tmp.append(e)
+  ret.append(tmp)
+  return ret
 
 seeds = tuple(map(int, sys.stdin.readline().strip().split(":")[-1].split()))
-for l in [x.strip() for x in sys.stdin.readlines()]:
-  print(l)
+fmap = None
+for table in reversed([[tuple(map(int, y.split())) for y in x[1:]] for x in split([x.strip() for x in sys.stdin.readlines()], "")]): fmap = Map(table, fmap)
+assert fmap is not None
+print(min(map(fmap.resolve, seeds)))
