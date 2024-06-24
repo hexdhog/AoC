00d7a206c9171c9f0d96068339542517a20bcaad
hexdhog <hexdhog@gmail.com>
2024-06-24 12:49:11 +0200

    2023 10p01

---
+++ 2023/10p01.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import sys
+
+from collections import deque
+
+matrix = [list(x.strip()) for x in sys.stdin.readlines()]
+N = len(matrix) * len(matrix[0])
+adj = [[0] * N for _ in range(N)]
+
+COORD = (-1, 0), (1, 0), (0, 1), (0, -1)
+SRC = ("S", "|", "L", "J"), ("S", "|", "7", "F"), ("S", "-", "L", "F"), ("S", "-", "7", "J")
+DST = ("|", "7", "F"), ("|", "L", "J"), ("-", "7", "J"), ("-", "L", "F")
+
+start = 0
+for y in range(len(matrix)):
+  for x in range(len(matrix[y])):
+    if matrix[y][x] == ".": continue
+    if matrix[y][x] == "S": start = y * len(matrix[0]) + x
+    for i in range(len(COORD)):
+      adjy, adjx = y + COORD[i][0], x + COORD[i][1]
+      if not all(0 <= x < len(matrix) for x in (adjx, adjy)): continue
+      if matrix[y][x] in SRC[i] and matrix[adjy][adjx] in DST[i]: adj[y * len(matrix[0]) + x][adjy * len(matrix[0]) + adjx] = 1
+
+def bfs(mat: list, root: int) -> list:
+  length, queue = [0] * len(mat), deque([root])
+  while queue:
+    node = queue.popleft()
+    for i in range(len(mat[node])):
+      if mat[node][i] == 1 and length[i] == 0:
+        length[i] = length[node] + 1
+        queue.append(i)
+  return length
+
+print(max(bfs(adj, start)))
