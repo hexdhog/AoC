ddf96b2a1ec3974af0a36119f3db398116d75f6f
hexdhog <hexdhog@gmail.com>
2024-07-07 12:48:43 +0200

    2023 16p01

---
+++ 2023/16p01.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+MOVS = {
+  "|": { (1, 0): ((0, 1), (0, -1)), (-1, 0): ((0, 1), (0, -1)) },
+  "-": { (0, 1): ((1, 0), (-1, 0)), (0, -1): ((1, 0), (-1, 0)) },
+  "/": { (1, 0): ((0, -1),), (-1, 0): ((0, 1),), (0, 1): ((-1, 0),), (0, -1): ((1, 0),) },
+  "\\": { (1, 0): ((0, 1),), (-1, 0): ((0, -1),), (0, 1): ((1, 0),), (0, -1): ((-1, 0),) }
+}
+
+grid = [list(x.strip()) for x in sys.stdin.readlines()]
+gvis = [[set() for _ in range(len(grid[0]))] for _ in range(len(grid))] # type: ignore[var-annotated]
+
+def bounds(x: int, y: int) -> bool: return all(0 <= a < b for a, b in ((x, len(grid[0])), (y, len(grid))))
+
+beams = [(0, 0, 1, 0)]
+gvis[0][0].add((1, 0))
+while len(beams) > 0:
+  tmp = [] # type: ignore[var-annotated]
+  for beam in beams:
+    x, y, dx, dy = beam
+    if dirs := MOVS.get(grid[y][x], {}).get((dx, dy), None): tmp += [(x+d[0], y+d[1], *d) for d in dirs] # type: ignore[attr-defined]
+    else: tmp.append((x+dx, y+dy, dx, dy))
+  beams.clear()
+  for x, y, dx, dy in tmp:
+    if not bounds(x, y) or (dx, dy) in gvis[y][x]: continue
+    gvis[y][x].add((dx, dy))
+    beams.append((x, y, dx, dy))
+
+print(sum(int(len(y) > 0) for x in gvis for y in x))
