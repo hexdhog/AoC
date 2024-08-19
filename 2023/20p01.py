4628bc87075692b67c7da9a3c9a259ace4f60302
hexdhog <hexdhog@gmail.com>
2024-08-19 12:17:54 +0200

    20p01 complete

--- 2023/20p01.py
+++ 2023/20p01.py
@@ -5,52 +5,44 @@ from __future__ import annotations
 import sys
 
 class Module:
-  def __init__(self, mtype: str, childs: list, parents: list): self.mtype, self.childs, self.parents, self.state = mtype, childs, parents, mtype == "&"
-  def process(self) -> None | bool:
-    if self.mtype == "%": return None if any(p.state for p in self.parents) else not self.state
+  def __init__(self, name: str, mtype: str, childs: list, parents: list):
+    self.name, self.mtype, self.childs, self.parents, self.state = name, mtype, childs, parents, False
+  def init(self): self.state = self.mtype == "&" and not all(p.state for p in self.parents)
+  def process(self, src: Module) -> None | bool:
+    if self.mtype == "%": return not self.state if not src.state else None
     if self.mtype == "&": return not all(p.state for p in self.parents)
     return self.state
 
-mods = {}
+mods, childs = { "out": Module("out", "", [], []) }, {}
 for l in sys.stdin.readlines():
   a, b = l.strip().split(" ", 1)
   name, mtyp = a if (bcst := a == "broadcaster") else a[1:], a if bcst else a[0]
-  mods[name] = Module(mtyp, b[3:].split(", "), [])
+  mods[name], childs[name] = Module(name, mtyp, [], []), b[3:].split(", ")
 
-for mod in mods.values():
-  for child in mod.childs:
-    if child in mods: mods[child].parents.append(mod)
+for mod, chlds in ((mods[n], [mods.get(c, mods["out"]) for c in childs[n]]) for n in mods if n in childs):
+  mod.childs = chlds
+  for c in chlds: c.parents.append(mod)
+for mod in mods.values(): mod.init()
 
 def cycle():
   lcnt, hcnt = 1, 0
-  upd = ["broadcaster"]
+  upd = [mods["broadcaster"]]
   while len(upd) > 0:
     tmp = []
-    for n, m, s in zip(upd, modl := [mods[n] for n in upd if n in mods], [m.process() for m in modl]):
-      if s is None: continue
-      lcnt, hcnt = lcnt+int(not s)*len(m.childs), hcnt+int(s)*len(m.childs)
-      m.state = s
-      for child in m.childs:
-        tmp.append(child)
-        print(f"{n} -{'high' if m.state else 'low'}-> {child}")
+    for src in upd:
+      lcnt, hcnt = lcnt+int(not src.state)*len(src.childs), hcnt+int(src.state)*len(src.childs)
+      for d, s in zip(src.childs, (d.process(src) for d in src.childs)):
+        if s is None: continue
+        d.state = s
+        tmp.append(d)
     upd = tmp
-  print()
   return lcnt, hcnt
 
 def snap(): return { name: mod.state for name, mod in mods.items() }
 
-org, i = snap(), 1
-print(org)
+NCYCLES = 1000
+org, i = snap(), 0
 lcnt, hcnt = cycle()
-print(snap())
-cycle()
-print(snap())
-# print(lcnt, hcnt)
-# while snap() != org:
-#   l, h = cycle()
-#   print(l, h)
-#   lcnt, hcnt, i = lcnt+l, hcnt+h, i+1
-#
-# lcnt = lcnt * (1000//i)
-# hcnt = hcnt * (1000//i)
-# print(lcnt, hcnt)
+while (i := i+1) < NCYCLES and snap() != org: lcnt, hcnt = (a+b for a, b in zip((lcnt, hcnt), cycle()))
+lcnt, hcnt = lcnt*(NCYCLES//i), hcnt*(NCYCLES//i) # TODO: what if i is not a multiple of NCYCLES?
+print(f"cycles: {i}; low: {lcnt}; high: {hcnt}; total: {lcnt*hcnt}")
