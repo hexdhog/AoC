a9aa5bef7d2c7657a7c03e881c2388fb14a5bb5c
hexdhog <hexdhog@gmail.com>
2024-09-14 13:49:39 +0200

    14p02 done

--- 2023/14p01.py
+++ 2023/14p01.py
@@ -4,4 +4,4 @@ import sys
 
 grid = list(map("".join, zip(*(l.strip() for l in sys.stdin))))
 grid = ["#".join("O" * (c := a.count("O")) + "." * (len(a) - c) for a in r.split("#")) for r in grid]
-print(sum(sum(len(grid)-i for i, v in enumerate(r) if v == "O") for r in grid))
+print(sum(len(grid)-i for r in grid for i, v in enumerate(r) if v == "O"))
--- 2023/14p02.py
+++ 2023/14p02.py
@@ -2,23 +2,22 @@
 
 import sys
 
-grid = [l.strip() for l in sys.stdin]
+grid = tuple(l.strip() for l in sys.stdin)
 
-print("\n".join("".join(x) for x in grid), end="\n\n")
+def load(grid): return sum((len(grid) - i) * r.count("O") for i, r in enumerate(grid))
+def cycle(grid):
+  for _ in range(4):
+    grid = tuple("".join(reversed(x)) for x in zip(*grid))
+    grid = tuple("#".join("." * ((len(a)) - (c := a.count("O"))) + "O" * c for a in r.split("#")) for r in grid)
+  return grid
 
-grid = list(map("".join, zip(*grid)))
+N = 1000000000
+seen = [grid]
+for _ in range(N):
+  if (grid := cycle(grid)) in seen: break
+  seen.append(grid)
 
-grid = ["#".join("O" * (c := a.count("O")) + "." * (len(a) - c) for a in r.split("#")) for r in grid]
-
-s = sum(sum(len(grid)-i for i, v in enumerate(r) if v == "O") for r in grid)
-
-grid = list(map("".join, zip(*grid)))
-print("\n".join("".join(x) for x in grid), end="\n\n")
-
-print(s)
-
-#  y  x
-# -1  0
-#  0 -1
-#  1  0
-#  0  1
+cnt, off = len(seen), seen.index(grid)
+itr_left, rep_cnt = N - off, cnt - off
+r = itr_left % rep_cnt
+print(load(seen[off+r]))
